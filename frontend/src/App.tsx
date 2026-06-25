76035ad3c1c4845e675fabe1eb0eb8a25d3715fb
showemeowkx <sashacherepov55@gmail.com>
2026-06-25 16:14:01 +0200

    Feat: add QuizListPage component to display and manage quizzes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import QuizDetailPage from "../pages/QuizDetailsPage";
+import QuizListPage from "../pages/QuizListPage";
 
 export default function App() {
   return (
@@ -7,6 +8,7 @@ export default function App() {
       <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Navigate to="/quizzes" replace />} />
+          <Route path="/quizzes" element={<QuizListPage />} />
           <Route path="/quizzes/:id" element={<QuizDetailPage />} />
         </Routes>
       </div>
